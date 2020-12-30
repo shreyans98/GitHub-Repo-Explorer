b4b5b46d7d5a0ffda98b1f55be9cddcf87200318
Shreyans Singhi <42538991+shreyans98@users.noreply.github.com>
2020-12-30 13:48:19 +0530

    Create theme.js

---
+++ src/theme.js
@@ -0,0 +1,14 @@
+const theme = {
+    typography: {
+        fontFamily: 'Raleway, Helvetica, sans-serif',
+    },
+
+    colors: {
+        primaryColor: '#008000',
+        titleColor: 'white'
+    },
+
+    secondaryColor: 'mediumseagreen',
+};
+
+export default theme;
